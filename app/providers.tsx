0d94a62658d25134c2257352e38ea2337f18ddfe
FrozenArcher <yhy242989105@hotmail.com>
2024-08-30 17:04:34 +0800

    add next UI

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+// nextui
+import { NextUIProvider } from "@nextui-org/react";
+
+// wallets
 import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
 import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
 import { createConfig, WagmiProvider } from "wagmi";
@@ -10,6 +14,7 @@ import { mainnet, confluxESpaceTestnet } from "viem/chains";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
 import { SolanaWalletConnectors } from "@dynamic-labs/solana";
 import { PropsWithChildren } from "react";
+
 
 const config = createConfig({
   chains: [
@@ -27,21 +32,23 @@ const queryClient = new QueryClient();
 
 export function Providers(props: PropsWithChildren) {
   return (
-    <DynamicContextProvider
-      settings={{
-        // Find your environment id at https://app.dynamic.xyz/dashboard/developer
-        environmentId: "f0f2bb32-6435-40c3-8b73-768d1c6f577a",
-        walletConnectors: [
-          EthereumWalletConnectors,
-          // SolanaWalletConnectors,
-        ],
-      }}
-    >
-      <WagmiProvider config={config}>
-        <QueryClientProvider client={queryClient}>
-          <DynamicWagmiConnector>{props.children}</DynamicWagmiConnector>
-        </QueryClientProvider>
-      </WagmiProvider>
-    </DynamicContextProvider>
+    <NextUIProvider>
+      <DynamicContextProvider
+        settings={{
+          // Find your environment id at https://app.dynamic.xyz/dashboard/developer
+          environmentId: "f0f2bb32-6435-40c3-8b73-768d1c6f577a",
+          walletConnectors: [
+            EthereumWalletConnectors,
+            // SolanaWalletConnectors,
+          ],
+        }}
+      >
+        <WagmiProvider config={config}>
+          <QueryClientProvider client={queryClient}>
+            <DynamicWagmiConnector>{props.children}</DynamicWagmiConnector>
+          </QueryClientProvider>
+        </WagmiProvider>
+      </DynamicContextProvider>
+    </NextUIProvider>
   );
 }
