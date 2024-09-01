b1da18e6fa3b9c5e8d3bbe4a87f4f1d18d3edfa2
FrozenArcher <yhy242989105@hotmail.com>
2024-09-01 23:21:58 +0800

    fix style

--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -3,7 +3,7 @@ import SideBar from './sidebar';
 
 export default function GamePage() {
   return (
-    <div className="w-full h-full flex flex-row">
+    <div className="w-full h-full flex flex-row gap-8">
       <div className="w-1/2 h-full flex flex-col items-center justify-center">
         <div className='bg-white h-52 w-52'></div>
         <div className='h-8'></div>
