7f83c05f5c3ad6374cac3a2e2f30138b67d37f92
KelsenL <shangweiliu66@gmail.com>
2024-09-04 21:51:39 +0800

    message

--- app/(main)/home/bag.tsx
+++ app/(main)/home/bag.tsx
@@ -1,26 +1,11 @@
-import TraitsDisplay from "@/app/components/traitsDisplay"
+import NFTDisplay from "@/app/components/nftDisplay"
 export default function Bag() {
     return (
         <div>
             <span>Bag</span>
             <span>TraitsList</span>
             <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                <TraitsDisplay />
-                <TraitsDisplay />
-                <TraitsDisplay />
-                <TraitsDisplay />
-                <TraitsDisplay />
-                <TraitsDisplay />
-                <TraitsDisplay />
-                <TraitsDisplay />
-                <TraitsDisplay />
-                <TraitsDisplay />
-                <TraitsDisplay />
-                <TraitsDisplay />
-                <TraitsDisplay />
-                <TraitsDisplay />
-                <TraitsDisplay />
-                <TraitsDisplay />
+                <NFTDisplay ButtonType = "equip or sell" />
             </div>
         </div>
     )
--- app/components/nftDisplay.tsx
+++ app/components/nftDisplay.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Image, useDisclosure} from '@nextui-org/react';
 import logo from "../assets/logo.png";
-export default function TraitsDisplay() {
+export default function NFTDisplay(props) {
     const {isOpen, onOpen, onOpenChange} = useDisclosure();
 
     return (
@@ -11,7 +11,7 @@ export default function TraitsDisplay() {
                 height={100}
                 alt="trait1 with fallback"
                 src={logo.src}
-                fallbacksrc={logo.src}
+                fallbackSrc={logo.src}
                 isZoomed
                 onClick={onOpen}
             />
@@ -29,14 +29,15 @@ export default function TraitsDisplay() {
                                     height={200}
                                     alt="trait1 with fallback"
                                     src={logo.src}
-                                    fallbacksrc={logo.src}
+                                    fallbackSrc={logo.src}
                                 />
                             </ModalHeader>
                             <ModalBody>
                                 <p>Traits Description</p>
                             </ModalBody>
                             <ModalFooter>
-                                <Button onPress={onClose}>Equip</Button>
+                                {props.ButtonType == "buy or auction" && <Button onPress={onClose}>buy or auction</Button>}
+                                {props.ButtonType == "equip or sell" && <Button onPress={onClose}>equip or sell</Button>}
                             </ModalFooter>
                         </>
                     )}
