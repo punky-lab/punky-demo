0b3ad6723f05164c131a43cfd0b157a3daecbd3a
KelsenL <shangweiliu66@gmail.com>
2024-09-05 13:30:42 +0800

    perfected the css

---
+++ app/(main)/marketplace/itemSales.tsx
@@ -0,0 +1,13 @@
+import NFTDisplay from "@/app/components/nftDisplay";
+interface BagProps{
+    ButtonType: string;
+}
+export default function ItemSales(_: BagProps) {
+    return (
+        <div>
+            <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                <NFTDisplay ButtonType = "buy or auction" />
+            </div>
+        </div>
+    )
+}
---
+++ app/components/nftDisplay.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Image, useDisclosure} from '@nextui-org/react';
+import logo from "../assets/logo.png";
+export default function NFTDisplay(props) {
+    const {isOpen, onOpen, onOpenChange} = useDisclosure();
+
+    return (
+        <>
+            <Image
+                width={100}
+                height={100}
+                alt="trait1 with fallback"
+                src={logo.src}
+                fallbackSrc={logo.src}
+                isZoomed
+                onClick={onOpen}
+            />
+            <Modal
+                isOpen={isOpen}
+                onOpenChange={onOpenChange}
+                classNames={{
+                    body: "py-6",
+                    backdrop: "bg-[#292f46]/50 backdrop-opacity-40",
+                    base: "border-[#292f46] bg-[#19172c] dark:bg-[#19172c] text-[#a8b0d3]",
+                    header: "border-b-[1px] border-[#292f46]",
+                    footer: "border-t-[1px] border-[#292f46]",
+                    closeButton: "hover:bg-white/5 active:bg-white/10",
+                }}
+            >
+                <ModalContent>
+                    {(onClose) => (
+                        <div className='flex flex-col items-center'>
+                            <ModalHeader>
+                                <div className='flex flex-col items-center'>
+                                    <h1>Trait Name</h1>
+                                    <Image
+                                        width={175}
+                                        height={175}
+                                        alt="trait1 with fallback"
+                                        src={logo.src}
+                                        fallbackSrc={logo.src}
+                                    />
+                                </div>
+                            </ModalHeader>
+                            <ModalBody>
+                                <p>Traits Description</p>
+                            </ModalBody>
+                            <ModalFooter>
+                                {props.ButtonType == "buy or auction" && <Button onPress={onClose}>buy</Button>}
+                                {props.ButtonType == "buy or auction" && <Button onPress={onClose}>auction</Button>}
+                                {props.ButtonType == "equip or sell" && <Button onPress={onClose}>equip</Button>}
+                                {props.ButtonType == "equip or sell" && <Button onPress={onClose}>sell</Button>}
+                            </ModalFooter>
+                        </div>
+                    )}
+                </ModalContent>
+            </Modal>
+        </>
+    );
+}
